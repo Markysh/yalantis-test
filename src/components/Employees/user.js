889026ba60d1d7b3ca8e80e302f406c50993e99e
Olya Markysh <december.olka51@gmail.com>
2020-11-27 18:29:27 +0200

    select users

--- src/components/Employees/user.js
+++ src/components/Employees/user.js
@@ -1,12 +1,24 @@
 import React from "react";
 import styles from "./emloyees.module.scss";
+import { useDispatch } from "react-redux";
+import { selectUser, removeUser } from "../../store/selected-users";
 
 export const User = (props) => {
   const user = props.user;
+  const dispatch = useDispatch();
+
+  const onChange = (event) => {
+    if (event.target.checked) {
+      dispatch(selectUser(user));
+    } else {
+      dispatch(removeUser(user));
+    }
+  };
+
   return (
     <div className={styles.sectionName} key={user.id}>
       {user.lastName} {user.firstName}
-      <input type="checkbox" />
+      <input type="checkbox" onChange={onChange} />
     </div>
   );
 };
--- src/components/EmployeesBirthday/employees-birthday.js
+++ src/components/EmployeesBirthday/employees-birthday.js
@@ -3,7 +3,7 @@ import styles from "./employees-birthday.module.scss";
 import { useSelector } from "react-redux";
 
 export const BirthdayList = (props) => {
-  const users = useSelector((state) => state.users).map((user) => {
+  const users = useSelector((state) => state.selectedUsers).map((user) => {
     return (
       <div className={styles.section} key={user.id}>
         <div className={styles.sectionName}>
